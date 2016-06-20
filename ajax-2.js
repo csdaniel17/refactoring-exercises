b2c7ada54b03ae62dfdd0e87a7ac1ddbf4ffe7c5
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-20 15:32:11 -0400

    ajax refactor

--- ajax-2.js
+++ ajax-2.js
@@ -17,26 +17,8 @@ function main() {
             map: map
           });
           marker.addListener('click', function() {
-            $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather',
-              method: 'GET',
-              data: {
-                q: city.name,
-                units: 'imperial',
-                APPID: 'eac2948bfca65b78a8c5564ecf91d00e'
-              },
-              success: function(data) {
-                var content = '<h1>' + data.name + '</h1>' +
-                  'Temperature: ' + data.main.temp + '°<br>' +
-                  'Hi: ' + data.main.temp_max + '°<br>' +
-                  'Lo: ' + data.main.temp_min + '°<br>' +
-                  data.weather[0].description + '<br>' +
-                  '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
-                var infoWindow = new google.maps.InfoWindow({
-                  content: content
-                });
-                infoWindow.open(map, marker);
-              }
+            getWeatherInfo(city, map, function(data) {
+              infoWindowInfo(data, marker);
             });
           });
         });
@@ -44,3 +26,33 @@ function main() {
     }
   });
 }
+
+function getWeatherInfo(city, map, callback) {
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather',
+    method: 'GET',
+    data: {
+      q: city.name,
+      units: 'imperial',
+      APPID: 'eac2948bfca65b78a8c5564ecf91d00e'
+    },
+    success: function(data) {
+      callback(data);
+    }
+  });
+}
+
+function infoWindowInfo(data, marker) {
+  var content = '<h1>' + data.name + '</h1>' +
+    'Temperature: ' + data.main.temp + '°<br>' +
+    'Hi: ' + data.main.temp_max + '°<br>' +
+    'Lo: ' + data.main.temp_min + '°<br>' +
+    data.weather[0].description + '<br>' +
+    '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png">';
+  var infoWindow = new google.maps.InfoWindow({
+    content: content
+  });
+  infoWindow.open(map, marker);
+}
+
+main();
